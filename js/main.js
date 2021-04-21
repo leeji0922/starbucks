9bf933a9c4b5ef04457dfea3b36835a591deb6ad
leeji0922 <leeji0922@gmail.com>
2021-04-21 22:29:03 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () { // 돋보기 모양을 클릭하였을때
-    searchInputEl.focus();
-});
-
-
-searchInputEl.addEventListener('focus', function() { //포커스가 됬을때
-    searchEl.classList.add('focused'); //특정요소에 클래스정보를 가지고있는 객체에서 추가하겠다.
-    searchInputEl.setAttribute('placeholder', '통합검색'); // html 인수를 지정할때
-});
-
-searchInputEl.addEventListener('blur', function() { //포커스가 해제됬을때
-    searchEl.classList.remove('focused'); //특정요소에 클래스정보를 가지고있는 객체에서 추가하겠다.
-    searchInputEl.setAttribute('placeholder', ''); // html 인수를 지정할때
-});
-
-
 /* 우측 배치 이미지 등장 기능 구현 */
 
 const badgeEl = document.querySelector('header .badges');
@@ -149,8 +130,3 @@ spyEls.forEach(function (spyEl) {
         .setClassToggle(spyEl, 'show')
         .addTo(new ScrollMagic.Controller());
 });
-
-
-// 날짜 계산
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021
